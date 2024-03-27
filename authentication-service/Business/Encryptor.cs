b492b3bfbb3da81ee28cb8b949eb2022222b9b8a
Coen <458416@student.fontys.nl>
2024-03-27 11:47:41 +0100

    Init commit authentication.service

---
+++ authentication-service/Business/Encryptor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography;
+
+namespace authentication_service.Business
+{
+	public class Encryptor
+	{
+		public byte[] salt()
+		{
+			byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+
+			return salt;
+		}
+
+		public void Hasher(string password)
+		{
+			 byte []_salt = salt();
+			string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: password,
+				salt: _salt,
+				prf: KeyDerivationPrf.HMACSHA256,
+				iterationCount: 1000000,
+				numBytesRequested: 256 / 8));
+		}
+	}
+}
---
+++ authentication-service/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using authentication_service.Business;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace authentication_service.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+		private readonly string ConnectionString;
+
+		public AuthController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+			ConnectionString = _configuration.GetSection("ConnectionStrings").GetValue<string>("DevDatabase");
+		}
+		// GET: api/<AuthController>
+
+		// GET api/<AuthController>/5
+		[HttpGet("/P/{password}")]
+		public string Get(string empty, string password)
+		{
+			Encryptor encryptor = new Encryptor();
+			//return encryptor.Hasher(password);
+			return "n/a";
+		}
+
+		[HttpGet("{salt}")]
+		public string Get(string salt)
+		{
+			Encryptor encryptor = new Encryptor();
+			//return encryptor.Hasher(Encoding.ASCII.GetBytes(salt), "SAMPLE");
+			return ConnectionString;
+		}
+
+		[HttpGet()]
+		public byte[] Get()
+		{
+			Encryptor encryptor = new Encryptor();
+			return encryptor.salt();
+		}
+
+		// POST api/<AuthController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<AuthController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<AuthController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
