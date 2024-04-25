e9d439329b106646b5c86e22722a34d14ad34118
Coen <458416@student.fontys.nl>
2024-04-25 15:03:07 +0200

    Iamstupid

---
+++ auth-tests/auth-tests/RabbitMq/Rabbitmqdummy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using authentication_service.Interfaces;
+using authentication_service.RabbitMq;
+
+namespace auth_tests.RabbitMq
+{
+	internal class Rabbitmqdummy : IRabbitMqManagement
+	{
+		public void SendMessage(RabbitMqMessageModel message)
+		{
+			
+		}
+	}
+}
